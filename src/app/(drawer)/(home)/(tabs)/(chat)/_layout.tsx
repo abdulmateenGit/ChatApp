a26d89a54ecf8dc00553a972feb58c95ea0292e3
Abdul <abdulmateen.lhr00@gmail.com>
2025-11-11 13:44:45 +0500

    Tabs updating

---
+++ src/app/(drawer)/(home)/(tabs)/(chat)/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function ChatLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={({ navigation }) => ({
+          title: "Chats",
+          headerTitleAlign: "center",
+          headerTransparent: true,
+          headerLeft: () => (
+            <Ionicons
+              onPress={() => navigation.openDrawer()}
+              name="menu-outline"
+              size={28}
+              className="px-1"
+              color="gray"
+            />
+          ),
+
+          headerRight: () => (
+            <Ionicons name="add" size={28} className="px-1" color="gray" />
+          ),
+        })}
+      />
+    </Stack>
+  );
+}
--- src/app/(drawer)/(home)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(home)/(tabs)/_layout.tsx
@@ -3,17 +3,36 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen
-        name="index"
+        name="(chat)"
         options={{
-          title: "Chat",
+          title: "Chats",
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="chatbox" size={size} color={color} />
+            <Ionicons name="chatbox" color={color} size={size} />
           ),
         }}
       />
-      <Tabs.Screen name="settings" options={{ title: "Setting" }} />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Setting",
+          headerTransparent: true,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerTransparent: true,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
